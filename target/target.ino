cf3766c381a514efb1484185b8bf374f46e7015d
seven320 <yosyuaomenw@yahoo.co.jp>
2019-11-10 20:15:56 +0900

    target stable

--- target/target.ino
+++ target/target.ino
@@ -4,25 +4,33 @@
 
 Servo servo1;
 
-const int RECV_PIN = 2;
+const int RECV_PIN = 7;
 const int LED0 = 13;
 const int LED1 = 12;
 const int LED2 = 11;
 const int LED3 = 10;
 
-const int SERVO = 6;
+const int SERVO = 3;
 const int SWITCH = 4;
 
 int mode = -1; // mode 0:手動モード 1:一定間隔モード 2:自動復帰モード 3:ゲームモード
 bool power = false;
 
 //button
-long button_power = 0xFFA25D; //reset
-long button_0 = 0xFF6897;
-long button_1 = 0xFF30CF;
-long button_2 = 0xFF18E7;
-long button_3 = 0xFF7A85;
+//long button_power = 0xFFA25D; //reset
+//long button_0 = 0xFF6897;
+//long button_1 = 0xFF30CF;
+//long button_2 = 0xFF18E7;
+//long button_3 = 0xFF7A85;
 
+
+//tv 
+long button_power = 0x81C750AF;
+long button_0 = 0x81C7807F;
+long button_1 = 0x81C740BF;
+long button_2 = 0x81C7C03F;
+long button_3 = 0x81C720DF;
+long con = 0xFFFFFFFF;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -30,15 +38,13 @@ decode_results results;
 void setup() {
   Serial.begin(9600);
   irrecv.enableIRIn(); //Start the receiver
-  pinMode(RECV_PIN, INPUT); //赤外線センサ
+  pinMode(RECV_PIN, INPUT_PULLUP); //赤外線センサ　pullupにより抵抗いらない
 
-  
   pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   
-
   servo1.attach(SERVO);
 }
 
@@ -65,40 +71,52 @@ void LED_all(bool voltage){
   digitalWrite(LED3, voltage);
 }
 
+
 void mode0(){ // 手動
+//  押したらターゲットが上がる
   bool cont = true;
   int old_val = 1;
   while(cont){
+    Serial.println("waiting mode1");
     if(irrecv.decode(&results)){
-      if(results.value == button_0){
+      long val = results.value;
+      if(val == button_0){
         if(old_val == 1){
+          Serial.println("target_on");
           target_on();
           old_val = 0;
-          irrecv.resume();
         }
+      }else if(val == con){
+        Serial.println("ffffffff");
       }else{
         cont = false;
       }
+      irrecv.resume();
     }else{
       old_val = 1;
     }
-  check_motor();
-  delay(20);
+    delay(40);
   }
 }
 
 void mode1(){
+//  時間で自動的に動作
   bool cont = true;
   unsigned long prev = millis();
   while(cont){
+    Serial.println("waiting mode2");
     if(irrecv.decode(&results)){
-      if(results.value != button_1){
-        cont = false;
+      if(results.value == button_1 || results.value == con){
+        cont = true;
+        Serial.println("mode2");
       }else{ // button_1 が押されたときは赤外をリセットしないと止まる
-        irrecv.resume();
+        cont = false;
+        Serial.println("change mode");
       }
+      irrecv.resume();
     }
-    if(millis()-prev > 500){
+    if(millis()-prev > 5000){
+      Serial.println("target on");
       target_on();
       prev = millis();
     }
@@ -120,6 +138,7 @@ void mode1(){
 void mode2(){
   bool cont = true;
   while(cont){
+//    スイッチが押されると上がる
     if(irrecv.decode(&results)){
       if(results.value != button_2){
         cont = false;
@@ -127,7 +146,8 @@ void mode2(){
         irrecv.resume();
       }
     }
-    if(digitalRead(SWITCH) == LOW){
+    if(digitalRead(SWITCH) == false){ 
+//      常にスイッチはHIGHで，接点を持つと電圧が０になる
       target_on();
     }
     delay(40);
@@ -148,7 +168,7 @@ void loop(){
         power = false;
       }
     }
-    if(power & results.value == button_0 | results.value == button_1 | results.value == button_2 | results.value == button_3){
+    if(power & (results.value == button_0 | results.value == button_1 | results.value == button_2 | results.value == button_3)){
       LED_all(LOW);
       if(results.value == button_0){
         digitalWrite(LED0, HIGH);
@@ -156,13 +176,14 @@ void loop(){
       }else if(results.value == button_1){
         digitalWrite(LED1, HIGH);
         mode = 1;
-      }else if(results.value == button_2){
-        digitalWrite(LED2, HIGH);
-        mode = 2;
-      }else if(results.value == button_3){
-        digitalWrite(LED3, HIGH);
-        mode = 3;
       }
+//      }else if(results.value == button_2){
+//        digitalWrite(LED2, HIGH);
+//        mode = 2;
+//      }else if(results.value == button_3){
+//        digitalWrite(LED3, HIGH);
+//        mode = 3;
+//      }
     }
     irrecv.resume();
   }
@@ -171,9 +192,10 @@ void loop(){
       mode0();
     }else if(mode == 1){
       mode1();
-    }else if(mode == 2){
-      mode2();
     }
+//    }else if(mode == 2){
+//      mode2();
+//    }
   }
   check_motor();
   delay(40);
