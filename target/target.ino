75eec0d2611313951e4b70570cc1a7f1ad9f1c98
seven320 <yosyuaomenw@yahoo.co.jp>
2019-11-13 02:13:54 +0900

    remove ir reciever bug

--- target/target.ino
+++ target/target.ino
@@ -1,3 +1,5 @@
+// 製作者　電電 Part time Soldiers 隊員
+
 #include <IRremoteInt.h> //for IR receiver
 #include <IRremote.h>
 #include<Servo.h> // for servo motor
@@ -13,24 +15,28 @@ const int LED3 = 10;
 const int SERVO = 3;
 const int SWITCH = 4;
 
-int mode = -1; // mode 0:手動モード 1:一定間隔モード 2:自動復帰モード 3:ゲームモード
+
+// mode 0:手動モード 
+// mode 1:一定間隔モード 
+// mode 2:自動復帰モード ハードを未実装
+// mode 3:ゲームモード　未実装
+int mode = -1; 
 bool power = false;
 
-//button
-//long button_power = 0xFFA25D; //reset
-//long button_0 = 0xFF6897;
-//long button_1 = 0xFF30CF;
-//long button_2 = 0xFF18E7;
-//long button_3 = 0xFF7A85;
+//ir
+//long ir_power = 0xFFA25D; //reset
+//long ir_0 = 0xFF6897;
+//long ir_1 = 0xFF30CF;
+//long ir_2 = 0xFF18E7;
+//long ir_3 = 0xFF7A85;
 
-
-//tv 
-long button_power = 0x81C750AF;
-long button_0 = 0x81C7807F;
-long button_1 = 0x81C740BF;
-long button_2 = 0x81C7C03F;
-long button_3 = 0x81C720DF;
-long con = 0xFFFFFFFF;
+//tv remote controller
+long ir_power = 0x81C750AF;
+long ir_0 = 0x81C7807F;
+long ir_1 = 0x81C740BF;
+long ir_2 = 0x81C7C03F;
+long ir_3 = 0x81C720DF;
+long ir_con = 0xFFFFFFFF;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -49,6 +55,7 @@ void setup() {
 }
 
 void target_on(){
+  Serial.println("target on!");
   servo1.write(20);
   delay(1000);
   servo1.write(180);
@@ -73,27 +80,21 @@ void LED_all(bool voltage){
 
 
 void mode0(){ // 手動
-//  押したらターゲットが上がる
+//  スイッチを押したらターゲットが上がるモード
   bool cont = true;
-  int old_val = 1;
   while(cont){
-    Serial.println("waiting mode1");
     if(irrecv.decode(&results)){
-      long val = results.value;
-      if(val == button_0){
-        if(old_val == 1){
-          Serial.println("target_on");
+      Serial.println(results.value, HEX);
+      long ir_signal = results.value;
+      if(ir_signal == ir_0 | ir_signal == ir_1 | ir_signal == ir_power){
+        if(ir_signal == ir_0){
           target_on();
-          old_val = 0;
+        }else{
+          Serial.println("change mode");
+          cont = false;
         }
-      }else if(val == con){
-        Serial.println("ffffffff");
-      }else{
-        cont = false;
       }
       irrecv.resume();
-    }else{
-      old_val = 1;
     }
     delay(40);
   }
@@ -104,62 +105,52 @@ void mode1(){
   bool cont = true;
   unsigned long prev = millis();
   while(cont){
-    Serial.println("waiting mode2");
     if(irrecv.decode(&results)){
-      if(results.value == button_1 || results.value == con){
-        cont = true;
-        Serial.println("mode2");
-      }else{ // button_1 が押されたときは赤外をリセットしないと止まる
-        cont = false;
-        Serial.println("change mode");
+      Serial.println(results.value, HEX);
+      long ir_signal = results.value;
+      if(ir_signal == ir_0 | ir_signal == ir_1 | ir_signal == ir_power){
+        if(ir_signal == ir_1){
+          Serial.println("mode2");
+        }else{
+          cont = false;
+          Serial.println("change mode");
+        }
       }
       irrecv.resume();
     }
-    if(millis()-prev > 5000){
-      Serial.println("target on");
+    if(cont && millis()-prev > 5000){
       target_on();
       prev = millis();
     }
     delay(40);
   }
 }
-//
+
 //void mode2(){
-//  while(true){
-//      if (digitalRead(4) == LOW) {
-//          digitalWrite(13, HIGH);
-//      } else {
-//          digitalWrite(13, LOW);
+//  bool cont = true;
+//  while(cont){
+////    スイッチが押されると上がる
+//    if(irrecv.decode(&results)){
+//      if(results.value != ir_2){
+//        cont = false;
+//      }else{
+//        irrecv.resume();
 //      }
+//    }
+//    if(digitalRead(SWITCH) == false){ 
+////      常にスイッチはHIGHで，接点を持つと電圧が０になる
+//      target_on();
+//    }
+//    delay(40);
 //  }
 //}
-
-
-void mode2(){
-  bool cont = true;
-  while(cont){
-//    スイッチが押されると上がる
-    if(irrecv.decode(&results)){
-      if(results.value != button_2){
-        cont = false;
-      }else{
-        irrecv.resume();
-      }
-    }
-    if(digitalRead(SWITCH) == false){ 
-//      常にスイッチはHIGHで，接点を持つと電圧が０になる
-      target_on();
-    }
-    delay(40);
-  }
-}
-
 
 void loop(){
   mode = -1;
   if(irrecv.decode(&results)){
     Serial.println(results.value, HEX);
-    if(results.value == button_power){
+    long ir_signal = results.value;
+    if(ir_signal == ir_power){
       if(power == false){ // power on 
         LED_all(HIGH);
         power = true;
@@ -168,21 +159,18 @@ void loop(){
         power = false;
       }
     }
-    if(power & (results.value == button_0 | results.value == button_1 | results.value == button_2 | results.value == button_3)){
+    if(power & (ir_signal == ir_0 | ir_signal == ir_1)){
       LED_all(LOW);
-      if(results.value == button_0){
+      if(ir_signal == ir_0){
         digitalWrite(LED0, HIGH);
         mode = 0;
-      }else if(results.value == button_1){
+      }else if(ir_signal == ir_1){
         digitalWrite(LED1, HIGH);
         mode = 1;
       }
-//      }else if(results.value == button_2){
+//      }else if(results.value == ir_2){
 //        digitalWrite(LED2, HIGH);
 //        mode = 2;
-//      }else if(results.value == button_3){
-//        digitalWrite(LED3, HIGH);
-//        mode = 3;
 //      }
     }
     irrecv.resume();
